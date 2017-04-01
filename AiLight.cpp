1f65ad58618f3608e86bf2f69b2dc12fb4238487
Sacha Telgenhof <stelgenhof@gmail.com>
2017-04-01 20:35:36 +0900

    Minor reformatting/cleanup.

--- AiLight.cpp
+++ AiLight.cpp
@@ -12,110 +12,136 @@
 
 #include "AiLight.hpp"
 
-AiLightClass::AiLightClass(void) {
-  _my9291 = new my9291(MY9291_DI_PIN, MY9291_DCKI_PIN, MY9291_COMMAND_DEFAULT);
+AiLightClass::AiLightClass(void)
+{
+    _my9291 = new my9291(MY9291_DI_PIN, MY9291_DCKI_PIN, MY9291_COMMAND_DEFAULT);
 
-  setRGBW(); // Initialize color channels
+    setRGBW(); // Initialize color channels
 }
 
-AiLightClass::AiLightClass(const AiLightClass &obj) {
-  _my9291 = new my9291(MY9291_DI_PIN, MY9291_DCKI_PIN, MY9291_COMMAND_DEFAULT);
-  *_my9291 = *obj._my9291;
+AiLightClass::AiLightClass(const AiLightClass &obj)
+{
+    _my9291 = new my9291(MY9291_DI_PIN, MY9291_DCKI_PIN, MY9291_COMMAND_DEFAULT);
+    *_my9291 = *obj._my9291;
 
-  setRGBW(); // Initialize color channels
+    setRGBW(); // Initialize color channels
 }
 
-AiLightClass::~AiLightClass(void) { delete _my9291; }
-
-uint8_t AiLightClass::getBrightness(void) { return _brightness; }
-
-void AiLightClass::setBrightness(uint8_t level) {
-  _brightness = (level <= MY9291_LEVEL_MAX)
-                    ? level
-                    : MY9291_LEVEL_MAX; // Clamp value to maximum
-
-  setRGBW();
-  setState(true);
+AiLightClass::~AiLightClass(void)
+{
+    delete _my9291;
 }
 
-bool AiLightClass::getState(void) { return _my9291->getState(); }
-
-void AiLightClass::setState(bool state) { _my9291->setState(state); }
-
-Color AiLightClass::getColor(void) { return _color; }
-
-void AiLightClass::setColor(uint8_t red, uint8_t green, uint8_t blue) {
-  _color.red = red;
-  _color.green = green;
-  _color.blue = blue;
-
-  setRGBW();
-  setState(true);
+uint8_t AiLightClass::getBrightness(void)
+{
+    return _brightness;
 }
 
-void AiLightClass::setWhite(uint8_t white) {
-  _color.white = white;
+void AiLightClass::setBrightness(uint8_t level)
+{
+    _brightness = (level <= MY9291_LEVEL_MAX)
+            ? level
+            : MY9291_LEVEL_MAX; // Clamp value to maximum
 
-  setRGBW();
-  setState(true);
+    setRGBW();
+    setState(true);
 }
 
-uint16_t AiLightClass::getColorTemperature(void) { return _colortemp; }
-
-void AiLightClass::setColorTemperature(uint16_t temperature) {
-  _colortemp = temperature;
-
-  int tmpKelvin = 1000000UL / temperature; // Convert from mired value
-
-  // Temperature must fall between 1000 and 40000 degrees
-  tmpKelvin = (tmpKelvin < 1000) ? 1000 : tmpKelvin;   // Clamp value to minimum
-  tmpKelvin = (tmpKelvin > 40000) ? 40000 : tmpKelvin; // Clamp value to maximum
-
-  tmpKelvin = tmpKelvin / 100; // All calculations require tmpKelvin \ 100, so
-  // only do the conversion once
-
-  // Perform conversions from color temperature to RGB values
-
-  // Red
-  _color.red = (tmpKelvin <= 66)
-                   ? MY9291_LEVEL_MAX
-                   : 329.698727446 * pow((tmpKelvin - 60), -0.1332047592);
-
-  _color.red = (_color.red > MY9291_LEVEL_MAX)
-                   ? MY9291_LEVEL_MAX
-                   : _color.red; // Clamp value to maximum
-
-  // Green
-  _color.green = (tmpKelvin <= 66)
-                     ? 99.4708025861 * log(tmpKelvin) - 161.1195681661
-                     : 288.1221695283 * pow(tmpKelvin, -0.0755148492);
-
-  _color.green = (_color.green > MY9291_LEVEL_MAX)
-                     ? MY9291_LEVEL_MAX
-                     : _color.green; // Clamp value to maximum
-
-  // Blue
-  _color.blue =
-      (tmpKelvin >= 66)
-          ? MY9291_LEVEL_MAX
-          : ((tmpKelvin <= 19)
-                 ? 0
-                 : 138.5177312231 * log(tmpKelvin - 10) - 305.0447927307);
-
-  _color.blue = (_color.blue > MY9291_LEVEL_MAX)
-                    ? MY9291_LEVEL_MAX
-                    : _color.blue; // Clamp value to maximum
-
-  setRGBW();
-  setState(true);
+bool AiLightClass::getState(void)
+{
+    return _my9291->getState();
 }
 
-void AiLightClass::setRGBW() {
-  _my9291->setColor((my9291_color_t){
-      (uint32_t)map(_color.red, 0, MY9291_LEVEL_MAX, 0, _brightness),
-      (uint32_t)map(_color.green, 0, MY9291_LEVEL_MAX, 0, _brightness),
-      (uint32_t)map(_color.blue, 0, MY9291_LEVEL_MAX, 0, _brightness),
-      (uint32_t)map(_color.white, 0, MY9291_LEVEL_MAX, 0, _brightness)});
+void AiLightClass::setState(bool state)
+{
+    _my9291->setState(state);
+}
+
+Color AiLightClass::getColor(void)
+{
+    return _color;
+}
+
+void AiLightClass::setColor(uint8_t red, uint8_t green, uint8_t blue)
+{
+    _color.red = red;
+    _color.green = green;
+    _color.blue = blue;
+
+    setRGBW();
+    setState(true);
+}
+
+void AiLightClass::setWhite(uint8_t white)
+{
+    _color.white = white;
+
+    setRGBW();
+    setState(true);
+}
+
+uint16_t AiLightClass::getColorTemperature(void)
+{
+    return _colortemp;
+}
+
+void AiLightClass::setColorTemperature(uint16_t temperature)
+{
+    _colortemp = temperature;
+
+    int tmpKelvin = 1000000UL / temperature; // Convert from mired value
+
+    // Temperature must fall between 1000 and 40000 degrees
+    tmpKelvin = (tmpKelvin < 1000) ? 1000 : tmpKelvin; // Clamp value to minimum
+    tmpKelvin = (tmpKelvin > 40000) ? 40000 : tmpKelvin; // Clamp value to maximum
+
+    tmpKelvin = tmpKelvin / 100; // All calculations require tmpKelvin \ 100, so
+    // only do the conversion once
+
+    // Perform conversions from color temperature to RGB values
+
+    // Red
+    _color.red = (tmpKelvin <= 66)
+            ? MY9291_LEVEL_MAX
+            : 329.698727446 * pow((tmpKelvin - 60), -0.1332047592);
+
+    _color.red = (_color.red > MY9291_LEVEL_MAX)
+            ? MY9291_LEVEL_MAX
+            : _color.red; // Clamp value to maximum
+
+    // Green
+    _color.green = (tmpKelvin <= 66)
+            ? 99.4708025861 * log(tmpKelvin) - 161.1195681661
+            : 288.1221695283 * pow(tmpKelvin, -0.0755148492);
+
+    _color.green = (_color.green > MY9291_LEVEL_MAX)
+            ? MY9291_LEVEL_MAX
+            : _color.green; // Clamp value to maximum
+
+    // Blue
+    _color.blue =
+            (tmpKelvin >= 66)
+            ? MY9291_LEVEL_MAX
+            : ((tmpKelvin <= 19)
+            ? 0
+            : 138.5177312231 * log(tmpKelvin - 10) - 305.0447927307);
+
+    _color.blue = (_color.blue > MY9291_LEVEL_MAX)
+            ? MY9291_LEVEL_MAX
+            : _color.blue; // Clamp value to maximum
+
+    setRGBW();
+    setState(true);
+}
+
+void AiLightClass::setRGBW()
+{
+    _my9291->setColor((my9291_color_t){
+        (uint32_t) map(_color.red, 0, MY9291_LEVEL_MAX, 0, _brightness),
+        (uint32_t) map(_color.green, 0, MY9291_LEVEL_MAX, 0, _brightness),
+        (uint32_t) map(_color.blue, 0, MY9291_LEVEL_MAX, 0, _brightness),
+        (uint32_t) map(_color.white, 0, MY9291_LEVEL_MAX, 0, _brightness)
+    });
 }
 
 AiLightClass AiLight;
--- AiLight.hpp
+++ AiLight.hpp
@@ -24,128 +24,127 @@
 
 // Structure for holding the levels of all the colour channels
 struct Color {
-  uint8_t red;
-  uint8_t green;
-  uint8_t blue;
-  uint8_t white;
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+    uint8_t white;
 };
 
 class AiLightClass {
-
 public:
-  AiLightClass(void);
-  AiLightClass(const AiLightClass &obj);
-  ~AiLightClass(void);
+    AiLightClass(void);
+    AiLightClass(const AiLightClass &obj);
+    ~AiLightClass(void);
 
-  /**
-   * @brief Returns the current state of the AiLight (i.e on or off)
-   *
-   * @return the current state of the AiLight
-   */
-  bool getState(void);
+    /**
+     * @brief Returns the current state of the AiLight (i.e on or off)
+     *
+     * @return the current state of the AiLight
+     */
+    bool getState(void);
 
-  /**
-   * @brief Sets the state of the AiLight (i.e on or off)
-   *
-   * @return void
-   */
-  void setState(bool state);
+    /**
+     * @brief Sets the state of the AiLight (i.e on or off)
+     *
+     * @return void
+     */
+    void setState(bool state);
 
-  /**
-   * @brief Returns the currently set level of brightness
-   *
-   * @return the currently set level of brightness
-   */
-  uint8_t getBrightness(void);
+    /**
+     * @brief Returns the currently set level of brightness
+     *
+     * @return the currently set level of brightness
+     */
+    uint8_t getBrightness(void);
 
-  /**
-   * @brief Sets the level for brightness
-   *
-   * This functions sets the level for brightness and switches on the AiLight.
-   *
-   * @return void
-   */
-  void setBrightness(uint8_t level);
+    /**
+     * @brief Sets the level for brightness
+     *
+     * This functions sets the level for brightness and switches on the AiLight.
+     *
+     * @return void
+     */
+    void setBrightness(uint8_t level);
 
-  /**
-   * @brief Returns the currently set levels of all 4 colour channels (RGBW)
-   *
-   * @return the set levels of all 4 colour channels (RGBW)
-   */
-  Color getColor(void);
+    /**
+     * @brief Returns the currently set levels of all 4 colour channels (RGBW)
+     *
+     * @return the set levels of all 4 colour channels (RGBW)
+     */
+    Color getColor(void);
 
-  /**
-   * @brief Sets the levels for the red, green and blue colour channels
-   *
-   * This functions set the individual levels for the red, green and blue colour
-   * channels and switches on the AiLight. Note that all four colour channels
-   * work in conjunction: i.e. the white colour channel is not changed when the
-   * levels of the RGB colour channels are changed.
-   *
-   * @return void
-   */
-  void setColor(uint8_t red, uint8_t green, uint8_t blue);
+    /**
+     * @brief Sets the levels for the red, green and blue colour channels
+     *
+     * This functions set the individual levels for the red, green and blue colour
+     * channels and switches on the AiLight. Note that all four colour channels
+     * work in conjunction: i.e. the white colour channel is not changed when the
+     * levels of the RGB colour channels are changed.
+     *
+     * @return void
+     */
+    void setColor(uint8_t red, uint8_t green, uint8_t blue);
 
-  /**
-   * @brief Sets the level for the white colour channel
-   *
-   * This functions set the individual level for white colour channel and
-   * switches on the AiLight. Note that all four colour channels work in
-   * conjunction: i.e. the RGB colour channels are not changed when the level of
-   * the white colour channel is changed.
-   *
-   * @return void
-   */
-  void setWhite(uint8_t white);
+    /**
+     * @brief Sets the level for the white colour channel
+     *
+     * This functions set the individual level for white colour channel and
+     * switches on the AiLight. Note that all four colour channels work in
+     * conjunction: i.e. the RGB colour channels are not changed when the level of
+     * the white colour channel is changed.
+     *
+     * @return void
+     */
+    void setWhite(uint8_t white);
 
-  /**
-   * @brief Returns the currently set colour temperature
-   *
-   * @return the currently set colour temperature (in mired)
-   */
-  uint16_t getColorTemperature(void);
+    /**
+     * @brief Returns the currently set colour temperature
+     *
+     * @return the currently set colour temperature (in mired)
+     */
+    uint16_t getColorTemperature(void);
 
-  /**
-   * @brief Sets the colour of the AiLight based on the given colour temperature
-   *
-   * This method sets the colour of the AiLight based on the given colour
-   * temperature and switches on the AiLight. The colour temperature is defined
-   * in mired (micro reciprocal degree, the mired is a unit of measurement used
-   * to express colour temperature). Subsequently, the colour temperature is
-   * converted to equivalent RGB values as based on the algorithm by Tanner
-   * Helland.
-   *
-   * Sources:
-   * http://www.tannerhelland.com/4435/convert-temperature-rgb-algorithm-code/
-   * https://en.wikipedia.org/wiki/Mired
-   *
-   */
-  void setColorTemperature(uint16_t temperature);
+    /**
+     * @brief Sets the colour of the AiLight based on the given colour temperature
+     *
+     * This method sets the colour of the AiLight based on the given colour
+     * temperature and switches on the AiLight. The colour temperature is defined
+     * in mired (micro reciprocal degree, the mired is a unit of measurement used
+     * to express colour temperature). Subsequently, the colour temperature is
+     * converted to equivalent RGB values as based on the algorithm by Tanner
+     * Helland.
+     *
+     * Sources:
+     * http://www.tannerhelland.com/4435/convert-temperature-rgb-algorithm-code/
+     * https://en.wikipedia.org/wiki/Mired
+     *
+     */
+    void setColorTemperature(uint16_t temperature);
 
 private:
-  my9291 *_my9291; // MY9291 driver handle
+    my9291 *_my9291; // MY9291 driver handle
 
-  // Current colour levels (RGBW). Initial values are 1/4th of maximum
-  Color _color = {MY9291_LEVEL_MAX >> 2, MY9291_LEVEL_MAX >> 2,
-                  MY9291_LEVEL_MAX >> 2, MY9291_LEVEL_MAX >> 2};
+    // Current colour levels (RGBW). Initial values are 1/4th of maximum
+    Color _color = {MY9291_LEVEL_MAX >> 2, MY9291_LEVEL_MAX >> 2,
+        MY9291_LEVEL_MAX >> 2, MY9291_LEVEL_MAX >> 2};
 
-  // Current brightness level. Initial value is 1/4th of maximum
-  uint8_t _brightness = MY9291_LEVEL_MAX >> 2;
+    // Current brightness level. Initial value is 1/4th of maximum
+    uint8_t _brightness = MY9291_LEVEL_MAX >> 2;
 
-  // Current colour temperature setting. Initial value is equivalent of 2700K
-  uint16_t _colortemp = 370;
+    // Current colour temperature setting. Initial value is equivalent of 2700K
+    uint16_t _colortemp = 370;
 
-  /**
-   * @brief Sets the levels of all colour levels (RGBW) to the MY9291 LED
-   * driver.
-   *
-   * This internal method sets the levels of all colour levels (RGBW) to the
-   * MY9291 LED driver including the brightness level. To switch on the AiLight,
-   * the setState() method needs to be used subsequently.
-   *
-   * @return void
-   */
-  void setRGBW();
+    /**
+     * @brief Sets the levels of all colour levels (RGBW) to the MY9291 LED
+     * driver.
+     *
+     * This internal method sets the levels of all colour levels (RGBW) to the
+     * MY9291 LED driver including the brightness level. To switch on the AiLight,
+     * the setState() method needs to be used subsequently.
+     *
+     * @return void
+     */
+    void setRGBW();
 };
 
 extern AiLightClass AiLight;
