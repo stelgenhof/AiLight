3e8b45ce2526650da4b751f13dcaee4276ccc601
Sacha Telgenhof <stelgenhof@gmail.com>
2017-04-26 16:21:12 +0900

    Reverted back MDNS hostname setting as multiple devices could run in AP mode which can cause conflicts.

--- src/_wifi.ino
+++ src/_wifi.ino
@@ -33,15 +33,14 @@ void setupWiFi() {
   if (WiFi.getMode() != WIFI_STA) {
     WiFi.mode(WIFI_STA);
     WiFi.setOutputPower(WIFI_OUTPUT_POWER);
+    delay(10);
   }
 
   // Connecting
   WiFi.disconnect();
   DEBUGLOG("[WIFI] Connecting to %s\n", cfg.wifi_ssid);
   WiFi.begin(cfg.wifi_ssid, cfg.wifi_psk);
-  if (MDNS.begin(WiFi.getMode() == WIFI_AP ? HOSTNAME : cfg.hostname)) {
-    MDNS.addService("http", "tcp", 80);
-  }
+  MDNS.addService("http", "tcp", 80);
 
   // Check connection and switch to AP mode if no connection
   if (WiFi.waitForConnectResult() != WL_CONNECTED) {
