42330a725bf2494f6cc1c95af4ddb52a038f0065
Sacha Telgenhof <stelgenhof@gmail.com>
2019-02-06 08:45:44 +0900

    Bumped version (dev)
    Signed-off-by: Sacha Telgenhof <stelgenhof@gmail.com>

--- src/main.h
+++ src/main.h
@@ -11,7 +11,7 @@
  */
 
 #define APP_NAME "AiLight"
-#define APP_VERSION "0.5.1-dev"
+#define APP_VERSION "0.6-dev"
 #define APP_AUTHOR "me@sachatelgenhof.com"
 
 #define DEVICE_MANUFACTURER "Ai-Thinker"
